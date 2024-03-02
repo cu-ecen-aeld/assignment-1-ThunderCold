13b919ca659c1b65bad722a1f85e85d32122b0db
ThunderCold <lilwen0620@gmail.com>
2024-03-02 23:30:16 +0800

    update assignment 1

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-writefile="$1"
-writestr="$2"
-
-if [ $# -lt 2 ] || [ ! -n $writestr ]
-then
+if [ "$#" -ne 2 ]; then
     echo "Error: Please provide the full path to a file and the text string to write."
     exit 1
 fi
 
-dir=$(dirname "$writefile")
+writefile="$1"
+writestr="$2"
 
-if [ $(touch "$writefile" 2> "/dev/null" || mkdir -p "$dir") ]; then
-    echo "Error"
+if [ -z "$writestr" ]; then
+    echo "Error: Write string is not specified."
     exit 1
 fi
 
+mkdir -p "$(dirname "$writefile")"
+
 echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ]; then
+    echo "Error: Could not create or write to the file '$writefile'."
+    exit 1
+else
+    echo "File '$writefile' created successfully with content: '$writestr'"
+fi
